b49936043bec1d88c352f5b39c8184184b51cd5c
sun-yryr <texiyo0425@yahoo.co.jp>
2017-11-14 07:50:06 +0900

    path変更

--- rec_agqr.sh
+++ rec_agqr.sh
@@ -1,8 +1,7 @@
 !/bin/sh
 
 DATE=`date +%Y_%m%d_%H%M`
-SAVE_sDIR=`date +%Y%m`
-SAVE_DIR="/home/pi/AGQR/${SAVE_sDIR}/"
+SAVE_DIR="/home/pi/hdd1/sun-mba/radio/“
 RECORDING_TIME=5
 
 RTMP_r='--rtmp "rtmpe://fms1.uniqueradio.jp/"'
@@ -13,11 +12,13 @@ RTMP_p='-p http://www.uniqueradio.jp/agplayerf/newplayerf2-win.php'
 RTMP_C='-C B:0'
 RTMP_y='-y aandg22'
 RTMP_B="--stop ${RECORDING_TIME}"
+
+#$1 > file name
 RTMP_o="-o ${SAVE_DIR}agqr-${DATE}.flv"
 RTMP_PARAM="${RTMP_r} ${RTMP_a} ${RTMP_f} ${RTMP_W} ${RTMP_p} ${RTMP_C} ${RTMP_y} ${RTMP_B} --live ${RTMP_o}"
 
-# echo agqr-${DATE}.flv
-echo "rtmpdump ${RTMP_PARAM}"
+#echo agqr-${DATE}.flv
+#echo "rtmpdump ${RTMP_PARAM}"
 
 #recoding start
 rtmpdump ${RTMP_PARAM}
