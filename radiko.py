602a44b1548297d0e6d3571e77d2795e9a7ac9fe
sun-yryr <taittide@gmail.com>
2019-05-07 06:38:16 +0900

    radiko_test

---
+++ radiko.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+import xml.etree.ElementTree as ET
+import re
+import base64
+import datetime as DT
+
+
+cast = re.compile("(三森すずこ|夏川椎菜)")
+
+class yradio:
+    RADIKO_URL = "http://radiko.jp/v3/program/today/JP13.xml"
+    def __init__(self):
+        res = requests.get(self.RADIKO_URL)
+        res.encoding = "utf-8"
+        self.isKeyword = False
+        self.program_radiko = ET.fromstring(res.text)
+
+    def reload_program(self):
+        res = requests.get(self.RADIKO_URL)
+        res.encoding = "utf-8"
+        self.program_radiko = ET.fromstring(res.text)
+    
+    def change_keywords(self, keywords):
+        if bool(keywords):
+            word = "("
+            for keyword in keywords:
+                word += keyword
+                word += "|"
+            word = word.rstrip("|")
+            word += ")"
+            print(word)
+            self.isKeyword = True
+            self.keyword = re.compile(word)
+        else:
+            self.isKeyword = False
+
+    def delete_keywords(self):
+        self.change_keywords([])
+        
+    def search(self):
+        if (self.isKeyword is False): return None
+        res = []
+        for station in self.program_radiko.findall("stations/station"):
+            for prog in station.findall("./progs/prog"):
+                ck = False
+                title = prog.find("title").text
+                info = prog.find("info").text
+                desc = prog.find("desc").text
+                pfm = prog.find("pfm").text
+                if (self.keyword.search(title)):    ck = True
+                if (ck is False) and (info is not None):
+                    if (self.keyword.search(info)): ck = True
+                if (ck is False) and (pfm is not None):
+                    if (self.keyword.search(pfm)):  ck = True
+                if (ck is False) and (desc is not None):
+                    if (self.keyword.search(desc)): ck = True
+                if (ck):
+                    res.append({
+                        "station": station.get("id"),
+                        "title": prog.find("title").text.replace(" ", "_"),
+                        "ft": prog.get("ft"),
+                        "DT_ft": DT.datetime.strptime(prog.get("ft"), "%Y%m%d%H%M%S"),
+                        "to": prog.get("to"),
+                        "ftl": prog.get("ftl"),
+                        "tol": prog.get("tol"),
+                        "dur": int(prog.get("dur"))
+                    })
+        if bool(res): return res
+        else: return None
+
+    def authorization(self):
+        auth1_url = "https://radiko.jp/v2/api/auth1"
+        auth2_url = "https://radiko.jp/v2/api/auth2"
+        auth_key = "bcd151073c03b352e1ef2fd66c32209da9ca0afa"
+        headers = {
+            "X-Radiko-App": "pc_html5",
+            "X-Radiko-App-Version": "0.0.1",
+            "X-Radiko-User": "sunyryr",
+            "X-Radiko-Device": "pc"
+        }
+        res = requests.get(auth1_url, headers=headers)
+        if (res.status_code != 200):
+            print("Authorization1 Failed")
+            return None
+        AuthToken = res.headers["X-RADIKO-AUTHTOKEN"]
+        KeyLength = int(res.headers["X-Radiko-KeyLength"])
+        KeyOffset = int(res.headers["X-Radiko-KeyOffset"])
+        tmp_authkey = auth_key[KeyOffset:KeyOffset+KeyLength]
+        AuthKey = base64.b64encode(tmp_authkey.encode('utf-8')).decode('utf-8')
+        #print(AuthKey)
+        headers = {
+            "X-Radiko-AuthToken": AuthToken,
+            "X-Radiko-PartialKey": AuthKey,
+            "X-Radiko-User": "sunyryr",
+            "X-Radiko-Device": "pc"
+        }
+        res = requests.get(auth2_url, headers=headers)
+        if (res.status_code == 200):
+            return AuthToken
+        else:
+            print("Authorization2 Failed")
+            return None
---
+++ rec.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import radiko
+import subprocess
+from multiprocessing import Process
+from signal import SIGINT
+import time
+import datetime as DT
+
+
+def main():
+    radio = radiko.yradio()
+    AuthToken = radio.authorization()
+    radio.change_keywords(["声優", "垣花正あなたとハッピー"])
+    res = radio.search()
+    com = DT.timedelta(seconds=20)
+    zero = DT.timedelta()
+    while(True):
+        now = DT.datetime.now()
+        print(res)
+        for i, data in enumerate(res):
+            tmp_time = data["DT_ft"] - now
+            if (tmp_time < zero):
+                res.remove(data)
+            elif (tmp_time < com):
+                print(data["ftl"])
+                p = Process(target=rec, args=([data, tmp_time.total_seconds(), AuthToken],))
+                p.start()
+                res.remove(data)
+        time.sleep(10)
+
+
+
+def rec(data):
+    program_data = data[0]
+    AuthToken = data[2]
+    print(program_data["title"])
+    cwd = ('ffmpeg -headers "X-Radiko-AuthToken: %s" -i "https://rpaa.smartstream.ne.jp/so/playlist.m3u8?station_id=%s&l=15&lsid=85605612400545823313125539756301319021&type=b" -acodec copy ./savefile/%s.m4a' % (AuthToken, program_data["station"], program_data["title"]+"_"+program_data["ft"][:12]))
+    p1 = subprocess.Popen(cwd, stdout=subprocess.DEVNULL, shell=True)
+    time.sleep(program_data["dur"]+data[1]-5)
+    #time.sleep(30)
+    p1.send_signal(SIGINT)
+
+if __name__ == "__main__":
+    main()
