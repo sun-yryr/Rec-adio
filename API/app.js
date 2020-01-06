ef96ebe8768c20bf10173db041909a21d24f65de
sun-yryr <taittide@gmailcom>
2020-01-06 13:17:39 +0900

    fix

--- API/app.js
+++ API/app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://radio.sun-yryr.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
