891619e9d7bb1730a9de5eac33f6c52035eef056
sun-yryr <taittide@gmail.com>
2020-04-29 04:36:27 +0900

    録音のテスト

--- test/test_agqr.py
+++ test/test_agqr.py
@@ -21,6 +21,33 @@ class TestAGQR(unittest.TestCase):
         self.Agqr.change_keywords(keywords)
         self.assertTrue(self.Agqr.keyword.search("麻倉もも"))
         self.assertFalse(self.Agqr.keyword.search("朝倉もも"))
+    
+    def test_rec(self):
+        import datetime as DT
+        from pathlib import Path
+        # 録音時に使うのは title と dur だけ
+        dummy_title = "test_rec_agqr"
+        dummy_data = {
+            "title": dummy_title,
+            "ft": "202004290000",
+            "DT_ft": DT.datetime.strptime("202004290000", "%Y%m%d%H%M"),
+            "to": "202004290000",
+            "dur": 0.09,
+            "pfm": "test"
+        }
+        savePath = Path().resolve() / "save"
+        print("aaa", str(savePath))
+        savePath.mkdir()
+        self.Agqr.rec([dummy_data, 0, str(savePath)])
+        # check file
+        fileName = dummy_title + "_" + dummy_data["ft"] + ".m4a"
+        path = savePath / dummy_title / fileName
+        self.assertTrue(path.is_file())
+        # remove file
+        path.unlink()
+        path.parent.rmdir()
+        savePath.rmdir()
+
 
 if __name__ == "__main__":
     unittest.main()
