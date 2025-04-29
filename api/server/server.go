42ea2d6b73915508d5dd151b18552d0c5b46a7be
sun-yryr <me@sun-yryr.com>
2025-04-29 18:00:19 +0900

    feat: /api/v1 prefixを追加

--- api/server/server.go
+++ api/server/server.go
@@ -9,9 +9,12 @@ import (
 func NewServer(routers ...router.Router) *gin.Engine {
 	engine := gin.Default()
 
+	// v1 APIグループを作成
+	v1 := engine.Group("/api/v1")
+
 	for _, router := range routers {
 		for _, route := range router.Routes() {
-			engine.Handle(route.Method(), route.Path(), route.Handler())
+			v1.Handle(route.Method(), route.Path(), route.Handler())
 		}
 	}
 
