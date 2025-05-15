f7bee0915925573da25d1b12dce0e6de3b6a0222
sun-yryr <me@sun-yryr.com>
2025-05-16 03:11:17 +0900

    refactor: 正規表現をパッケージ変数に切り出した

--- internal/fileutil/filename.go
+++ internal/fileutil/filename.go
@@ -6,22 +6,24 @@ import (
 	"strings"
 )
 
+var (
+	/* ファイル名に使用できない文字を正規表現で定義。
+	* Windows: \ / : * ? " < > |
+	* macOS: / :
+	* Linux: /
+	 */
+	invalidCharsPattern = regexp.MustCompile(`[\\/:*?"<>|]`)
+	// 連続するアンダースコアを1つにまとめる。
+	multipleUnderscoresPattern = regexp.MustCompile(`_+`)
+)
+
 // SanitizeFilename は、ファイル名に使用できない文字を安全な文字に置換する。
 // Windows、macOS、Linuxで共通して使用できない文字を処理する。
 func SanitizeFilename(filename string) string {
-	// ファイル名に使用できない文字を正規表現で定義
-	// Windows: \ / : * ? " < > |
-	// macOS: / :
-	// Linux: /
-	pattern := regexp.MustCompile(`[\\/:*?"<>|]`)
-
 	// 不正な文字をアンダースコアに置換
-	sanitized := pattern.ReplaceAllString(filename, "_")
-
+	sanitized := invalidCharsPattern.ReplaceAllString(filename, "_")
 	// 連続するアンダースコアを1つにまとめる
-	pattern = regexp.MustCompile(`_+`)
-	sanitized = pattern.ReplaceAllString(sanitized, "_")
-
+	sanitized = multipleUnderscoresPattern.ReplaceAllString(sanitized, "_")
 	sanitized = strings.TrimSpace(sanitized)
 
 	// 空文字列になった場合はデフォルト名を設定
