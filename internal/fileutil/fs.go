abc1e46a3a2b5d5c43a60f0849ec5edf60aaa63d
sun-yryr <me@sun-yryr.com>
2025-05-24 19:26:56 +0900

    fix: コメントの修正とnolintを使わない

--- internal/fileutil/fs.go
+++ internal/fileutil/fs.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/afero"
 )
 
-// FileSystem abstracts basic file operations.
+// FileSystem は基本的なファイル操作を抽象化します。
 type FileSystem interface {
 	Stat(name string) (os.FileInfo, error)
 	ReadFile(name string) ([]byte, error)
@@ -18,26 +18,23 @@ type FileSystem interface {
 	UserHomeDir() (string, error)
 }
 
-// aferoFS implements FileSystem using an underlying afero.Fs.
-type aferoFS struct {
+// AferoFS は基盤となるafero.Fsを使用してFileSystemを実装します。
+type AferoFS struct {
 	base afero.Fs
 }
 
-// NewOsFS returns a FileSystem implemented with the os filesystem.
-//
-//nolint:ireturn
-func NewOsFS() FileSystem {
-	return &aferoFS{base: afero.NewOsFs()}
+// NewOsFS はOSファイルシステムで実装されたAferoFSを返します。
+func NewOsFS() *AferoFS {
+	return &AferoFS{base: afero.NewOsFs()}
 }
 
-// NewMemFS returns a FileSystem implemented with an in-memory filesystem.
-//
-//nolint:ireturn
-func NewMemFS() FileSystem {
-	return &aferoFS{base: afero.NewMemMapFs()}
+// NewMemFS はインメモリファイルシステムで実装されたAferoFSを返します。
+func NewMemFS() *AferoFS {
+	return &AferoFS{base: afero.NewMemMapFs()}
 }
 
-func (fs *aferoFS) Stat(name string) (os.FileInfo, error) {
+// Stat は指定された名前のファイル情報を返します。
+func (fs *AferoFS) Stat(name string) (os.FileInfo, error) {
 	info, err := fs.base.Stat(name)
 	if err != nil {
 		return nil, errors.Wrap(err, "stat failed")
@@ -46,7 +43,8 @@ func (fs *aferoFS) Stat(name string) (os.FileInfo, error) {
 	return info, nil
 }
 
-func (fs *aferoFS) ReadFile(name string) ([]byte, error) {
+// ReadFile はファイル名で指定されたファイルを読み取り、その内容を返します。
+func (fs *AferoFS) ReadFile(name string) ([]byte, error) {
 	data, err := afero.ReadFile(fs.base, name)
 	if err != nil {
 		return nil, errors.Wrap(err, "read file failed")
@@ -55,7 +53,8 @@ func (fs *aferoFS) ReadFile(name string) ([]byte, error) {
 	return data, nil
 }
 
-func (fs *aferoFS) WriteFile(name string, data []byte, perm os.FileMode) error {
+// WriteFile は指定されたファイルにデータを書き込み、必要に応じてファイルを作成します。
+func (fs *AferoFS) WriteFile(name string, data []byte, perm os.FileMode) error {
 	if err := afero.WriteFile(fs.base, name, data, perm); err != nil {
 		return errors.Wrap(err, "write file failed")
 	}
@@ -63,7 +62,8 @@ func (fs *aferoFS) WriteFile(name string, data []byte, perm os.FileMode) error {
 	return nil
 }
 
-func (fs *aferoFS) MkdirAll(path string, perm os.FileMode) error {
+// MkdirAll は指定されたパスのディレクトリを、必要な親ディレクトリと共に作成します。
+func (fs *AferoFS) MkdirAll(path string, perm os.FileMode) error {
 	if err := fs.base.MkdirAll(path, perm); err != nil {
 		return errors.Wrap(err, "mkdir failed")
 	}
@@ -71,7 +71,8 @@ func (fs *aferoFS) MkdirAll(path string, perm os.FileMode) error {
 	return nil
 }
 
-func (fs *aferoFS) Remove(name string) error {
+// Remove は指定されたファイルまたはディレクトリを削除します。
+func (fs *AferoFS) Remove(name string) error {
 	if err := fs.base.Remove(name); err != nil {
 		return errors.Wrap(err, "remove file failed")
 	}
@@ -79,7 +80,8 @@ func (fs *aferoFS) Remove(name string) error {
 	return nil
 }
 
-func (fs *aferoFS) Rename(oldpath, newpath string) error {
+// Rename はoldpathをnewpathに名前変更（移動）します。
+func (fs *AferoFS) Rename(oldpath, newpath string) error {
 	if err := fs.base.Rename(oldpath, newpath); err != nil {
 		return errors.Wrap(err, "rename file failed")
 	}
@@ -87,7 +89,8 @@ func (fs *aferoFS) Rename(oldpath, newpath string) error {
 	return nil
 }
 
-func (fs *aferoFS) UserHomeDir() (string, error) {
+// UserHomeDir は現在のユーザーのホームディレクトリを返します。
+func (fs *AferoFS) UserHomeDir() (string, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		return "", errors.Wrap(err, "user home dir")
