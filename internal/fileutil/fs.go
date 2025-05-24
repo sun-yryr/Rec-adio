49dabe3e8237f78ac9491f309b915a966ad59d87
sun-yryr <me@sun-yryr.com>
2025-05-24 18:10:24 +0900

    refactor: change Fs -> base

--- internal/fileutil/fs.go
+++ internal/fileutil/fs.go
@@ -20,25 +20,25 @@ type FileSystem interface {
 
 // aferoFS implements FileSystem using an underlying afero.Fs.
 type aferoFS struct {
-	afero.Fs
+	base afero.Fs
 }
 
 // NewOsFS returns a FileSystem implemented with the os filesystem.
 //
 //nolint:ireturn
 func NewOsFS() FileSystem {
-	return &aferoFS{Fs: afero.NewOsFs()}
+	return &aferoFS{base: afero.NewOsFs()}
 }
 
 // NewMemFS returns a FileSystem implemented with an in-memory filesystem.
 //
 //nolint:ireturn
 func NewMemFS() FileSystem {
-	return &aferoFS{Fs: afero.NewMemMapFs()}
+	return &aferoFS{base: afero.NewMemMapFs()}
 }
 
 func (fs *aferoFS) Stat(name string) (os.FileInfo, error) {
-	info, err := fs.Fs.Stat(name)
+	info, err := fs.base.Stat(name)
 	if err != nil {
 		return nil, errors.Wrap(err, "stat failed")
 	}
@@ -47,7 +47,7 @@ func (fs *aferoFS) Stat(name string) (os.FileInfo, error) {
 }
 
 func (fs *aferoFS) ReadFile(name string) ([]byte, error) {
-	data, err := afero.ReadFile(fs.Fs, name)
+	data, err := afero.ReadFile(fs.base, name)
 	if err != nil {
 		return nil, errors.Wrap(err, "read file failed")
 	}
@@ -56,7 +56,7 @@ func (fs *aferoFS) ReadFile(name string) ([]byte, error) {
 }
 
 func (fs *aferoFS) WriteFile(name string, data []byte, perm os.FileMode) error {
-	if err := afero.WriteFile(fs.Fs, name, data, perm); err != nil {
+	if err := afero.WriteFile(fs.base, name, data, perm); err != nil {
 		return errors.Wrap(err, "write file failed")
 	}
 
@@ -64,7 +64,7 @@ func (fs *aferoFS) WriteFile(name string, data []byte, perm os.FileMode) error {
 }
 
 func (fs *aferoFS) MkdirAll(path string, perm os.FileMode) error {
-	if err := fs.Fs.MkdirAll(path, perm); err != nil {
+	if err := fs.base.MkdirAll(path, perm); err != nil {
 		return errors.Wrap(err, "mkdir failed")
 	}
 
@@ -72,7 +72,7 @@ func (fs *aferoFS) MkdirAll(path string, perm os.FileMode) error {
 }
 
 func (fs *aferoFS) Remove(name string) error {
-	if err := fs.Fs.Remove(name); err != nil {
+	if err := fs.base.Remove(name); err != nil {
 		return errors.Wrap(err, "remove file failed")
 	}
 
@@ -80,7 +80,7 @@ func (fs *aferoFS) Remove(name string) error {
 }
 
 func (fs *aferoFS) Rename(oldpath, newpath string) error {
-	if err := fs.Fs.Rename(oldpath, newpath); err != nil {
+	if err := fs.base.Rename(oldpath, newpath); err != nil {
 		return errors.Wrap(err, "rename file failed")
 	}
 
